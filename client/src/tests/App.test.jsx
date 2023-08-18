9d6107d0e5c721a3fba13bc0e83776fade55183a
James Preston <preston_104@hotmail.com>
2023-08-19 09:37:08 +1000

    adding in some name changes in the app.test.jsx

--- client/src/tests/App.test.jsx
+++ client/src/tests/App.test.jsx
@@ -10,34 +10,34 @@ import SignUpPage from '/Users/jamespreston/sei-course/classwork/Projects/Projec
 import LogoutPage from '../components/LogoutPage';
 
     // Test 1. This test ensures that the footer comment ("Created by James Preston") is rendered on HomePage.
-    it('1. renders the footer comment correctly', () => {
+    it('1. renders the footer comment correctly - Home Page', () => {
             const { getByText } = render(<HomePage />);
             expect(getByText('Created by James Preston')).toBeInTheDocument();
         });
 
     // Test 2. This test ensures that the footer comment ("Created by James Preston") is rendered on LoginPage.
-    it('2. renders the footer comment correctly', () => {
+    it('2. renders the footer comment correctly - Sign Up Page', () => {
         const { getByText } = render(<SignUpPage/>);
         expect(getByText('Created by James Preston')).toBeInTheDocument();
     });
 
 
     // Test 3. This test ensures that the footer comment ("Created by James Preston") is rendered on Information Page.
-    it('3. renders the footer comment correctly', () => {
+    it('3. renders the footer comment correctly - Information Page', () => {
         const { getByText } = render(<Information />);
         expect(getByText('Created by James Preston')).toBeInTheDocument();
     });
 
     
     // Test 4. This test ensures that the footer comment ("Created by James Preston") is rendered on LogoutPage.
-    it('5. renders the footer comment correctly', () => {
+    it('5. renders the footer comment correctly - Logout Page', () => {
         const { getByText } = render(<LogoutPage />);
         expect(getByText('Created by James Preston')).toBeInTheDocument();
     });
 
 
     // Test 5.  This test ensures that the footer comment ("Created by James Preston") is rendered on SearchPage.
-    it('5. renders the footer comment correctly', () => {
+    it('5. renders the footer comment correctly - Search Page', () => {
             const { getByText } = render(<SearchPage />);
             expect(getByText('Created by James Preston')).toBeInTheDocument();
         });
